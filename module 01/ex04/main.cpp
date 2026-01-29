ca9876ff30cdf94151fc688961a6b65389780557
anezka <havrankova.anezka@gmail.com>
2026-01-29 17:45:03 +0100

    checked m01e04, need to check file names later with previous exercises

--- module 01/ex04/main.cpp
+++ module 01/ex04/main.cpp
@@ -13,39 +13,59 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <bits/stdc++.h>
 
 //maybe add message if not found
 //close files afterwards
 int main(int argc, char *argv[])
 {
     if (argc != 4){
-        std::cerr << "Error. Arguments are incorrect\n";
+        std::cerr << "Error. Arguments are incorrect.\n";
         return 1;
     }
     std::string filename(argv[1]);
     std::string s1(argv[2]);
+    // if (s1.empty()){
+    //     std:cerr << "S1 cannot be empty\n";
+    //     return 1;
+    // }
     std::string s2(argv[3]);
-    std::string replaceFilename = filename;
-    replaceFilename = replaceFilename.insert(replaceFilename.length(), ".replace");
-    std::ofstream fileReplace(replaceFilename);
-    std::ifstream file(filename);
-    std::string line;
-    if (file.is_open()){
-        while (std::getline(file, line))
-        {
-            size_t position = line.find(s1);
-            if (position != std::string::npos)
-            {
-                line.erase(position, s1.length());
-                line.insert(position, s2);
-            }
-            fileReplace << line << std::endl;
-        }
-    }
-    else {
-        std::cerr << "Error opening file\n";
+    std::ifstream inputFile(filename.c_str());
+    if (!inputFile.is_open())
+    {
+        std::cerr << "Error openning the file\n";
         return 1;
     }
+    std::string wholeInput;
+    std::string line;
+    while(std::getline(inputFile, line))
+    {
+        wholeInput += line;
+        if (!inputFile.eof())
+            wholeInput += "\n";
+    }
+    inputFile.close();
+    std::string replaced;
+    size_t currentPosition = 0;
+    size_t position;
+    while(true)
+    {
+        position = wholeInput.find(s1, currentPosition);
+        if (position == std::string::npos)
+        {
+            replaced.append(wholeInput, currentPosition, std::string::npos);
+            break ;
+        }
+        replaced.append(wholeInput, currentPosition, position - currentPosition);
+        replaced += s2;
+        currentPosition = position + s1.length();
+    }
+    std::ofstream outputFile((filename + ".replace").c_str());
+    if (!outputFile.is_open())
+    {
+        std::cerr << "Error creating file.\n";
+        return 1;
+    }
+    outputFile << replaced;
+    outputFile.close();
     return 0;
 }
